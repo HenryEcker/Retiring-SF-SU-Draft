d94fd98d3b24bbfd14b5a1a877637a7ac3e20ed2
HenryEcker <hde1997@gmail.com>
2022-05-14 15:47:33 -0400

    Close Dialogue / Privileged Users Guidance Tweaks

--- builders/main.py
+++ builders/main.py
@@ -113,7 +113,7 @@ def main() -> None:
             rendered_html=(
                     html_output_folder / 'mock-privileged-banner-rendered.html'
             ),
-            output_img_size=(694 + (pbm_px * 2), 202 + (pbm_px * 2))
+            output_img_size=(694 + (pbm_px * 2), 219 + (pbm_px * 2))
         ),
         "public_banner": RenderConfig(
             html_template=templates_folder / 'mock-public-banner.html',
